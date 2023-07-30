91bcc64b8ec2dd7a9afa845e5ff87b8e1dfacd20
I-Varun <varunsingh04092001@gmail.com>
2023-07-31 00:23:51 +0530

    random commit

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -21,37 +21,61 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
           backgroundColor: const Color.fromARGB(255, 155, 236, 158),
           centerTitle: true,
         ),
-        body: Container(
-          color: const Color.fromARGB(255, 155, 236, 158),
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text(
-                  'We have already send a verification email.Please open it and verify your email'),
-              Text('If you have not received yet,resent email.... '),
-              SizedBox(height: 10),
-              TextButton(
-                onPressed: () async {
-                  await Authservice.firebase().sendEmailVerification();
-                  // Navigator.of(context).pushNamedAndRemoveUntil(
-                  //   loginroute,
-                  //   (route) => false,
-                  // );
-                },
-                child: const Text('Resend email Verification',
-                    style: TextStyle(fontSize: 15)),
-              ),
-              SizedBox(height: 10),
-              TextButton(
-                onPressed: () {
-                  Authservice.firebase().logOut();
-                  Navigator.of(context)
-                      .pushNamedAndRemoveUntil(registerroute, (route) => false);
-                },
-                child: const Text("Restart", style: TextStyle(fontSize: 15)),
-              )
-            ],
+        body: Center(
+          child: Container(
+            color: Color.fromARGB(255, 249, 249, 249),
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                    'We have already send a verification email.Please open it and verify your email'),
+                Text('If you have not received yet,resent email.... '),
+                SizedBox(height: 10),
+                // TextButton(
+                // onPressed: () async {
+                //   await Authservice.firebase().sendEmailVerification();
+                //   // Navigator.of(context).pushNamedAndRemoveUntil(
+                //   //   loginroute,
+                //   //   (route) => false,
+                //   // );
+                // },
+                FloatingActionButton.extended(
+                  extendedPadding: EdgeInsets.only(left: 150, right: 150),
+                  label: const Text('Resend email Verification',
+                      style: TextStyle(color: Color.fromRGBO(0, 0, 0, 0.923))),
+                  backgroundColor: Color.fromARGB(255, 248, 249, 248),
+                  onPressed: () async {
+                    await Authservice.firebase().sendEmailVerification();
+                    // Navigator.of(context).pushNamedAndRemoveUntil(
+                    //   loginroute,
+                    //   (route) => false,
+                    // );
+                  },
+                ),
+                // child: const Text('Resend email Verification',
+                //     style: TextStyle(fontSize: 15)),
+
+                SizedBox(height: 10),
+                FloatingActionButton.extended(
+                  extendedPadding: EdgeInsets.only(left: 150, right: 150),
+                  label: const Text('Restart',
+                      style: TextStyle(color: Color.fromRGBO(0, 0, 0, 0.923))),
+                  backgroundColor: Color.fromARGB(255, 248, 249, 248),
+                  onPressed: () async {
+                    Authservice.firebase().logOut();
+                  },
+                ),
+                // TextButton(
+                //   onPressed: () {
+                //     Authservice.firebase().logOut();
+                //     Navigator.of(context)
+                //         .pushNamedAndRemoveUntil(registerroute, (route) => false);
+                //   },
+                //   child: const Text("Restart", style: TextStyle(fontSize: 15)),
+                // )
+              ],
+            ),
           ),
         ));
   }
