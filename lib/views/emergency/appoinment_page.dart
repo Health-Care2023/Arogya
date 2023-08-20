6060b71d7dd4f841fbde5900dc44fd72fc05244b
Soutikmondal <69642549+Soutikmondal@users.noreply.github.com>
2023-08-20 23:17:34 +0530

    Search engine

--- lib/views/emergency/appoinment_page.dart
+++ lib/views/emergency/appoinment_page.dart
@@ -10,7 +10,6 @@ import '../pages/Gender_dropdown.dart';
 class AppointmentPage extends StatefulWidget {
   const AppointmentPage({super.key});
 
-  
   @override
   State<AppointmentPage> createState() => _AppointmentPageState();
 }
@@ -18,24 +17,45 @@ class AppointmentPage extends StatefulWidget {
 class _AppointmentPageState extends State<AppointmentPage> {
   int selectedIndex = -1;
   TextEditingController searchController = TextEditingController();
-List<String> allDoctorNames = [
-  // Add doctor names here
-  "Varun",
-  "Ankan",
-  "Parthib",
-  "Arka",
-  "Soutik"
-];
+  List<String> allDoctorNames = [
+    // Add doctor names here
+    "Varun",
+    "Ankan",
+    "Parthib",
+    "Arka",
+    "Soutik"
+  ];
+  List<Map<String, String>> doctorNames = [
+    {"name": "Varun", "specialist": "oncologist"},
+    {"name": "Soutik", "specialist": "cardiologist"},
+    {"name": "Parthib", "specialist": "padaetrician"},
+    {"name": "Arka", "specialist": "eye specialist"}
+  ];
 
-List<String> allSpecialties = [
-  // Add doctor specialties here
-  "oncologist",
-  "cardiologist",
-  "padaetrician",
-  "dermatologists",
-  "eye specialist"
-];
-List<String> filteredDoctorNames = [];
+  List<String> allSpecialties = [
+    // Add doctor specialties here
+    "oncologist",
+    "cardiologist",
+    "padaetrician",
+    "dermatologists",
+    "eye specialist"
+  ];
+  List<Map<String, String>> _foundUsers = [];
+  void _runFilter(String enteredKeyword) {
+    List<Map<String, String>> results = [];
+    if (enteredKeyword.isEmpty) {
+      results = doctorNames;
+    } else {
+      results = doctorNames.where((user) {
+        return user["name"]!.contains(enteredKeyword.toLowerCase());
+      }).toList();
+    }
+
+    setState(() {
+      _foundUsers = results;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
@@ -47,91 +67,77 @@ List<String> filteredDoctorNames = [];
   }
 
   @override
-Widget build(BuildContext context) {
-      return Container(
-        margin: const EdgeInsets.all(20),
-        
-        //  width: (MediaQuery.of(context).size.width),
-        // height: (MediaQuery.of(context).size.height),
-        // color: Colors.red,
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(20),
+
+      //  width: (MediaQuery.of(context).size.width),
+      // height: (MediaQuery.of(context).size.height),
+      // color: Colors.red,
       child: SingleChildScrollView(
-         scrollDirection: Axis.vertical,
+        scrollDirection: Axis.vertical,
         child: Column(
-            children:[
+          children: [
             //    SingleChildScrollView(
             //     scrollDirection: Axis.horizontal,
-                 
-                     Row(
-                       children: [
-                         Expanded(
 
-                            flex: 3,
-                           child: TextField(
-                            // onChanged: (value) => _runFilter(value),
-                            controller: searchController,
-                  onChanged: (value) {
-                    setState(() {
-                      // Filter suggestions based on entered text
-                       if (value.isEmpty) {
-                          filteredDoctorNames.clear();
-                        }
-                        else{
-                      filteredDoctorNames = allDoctorNames
-                          .where((name) =>
-                              name.toLowerCase().contains(value.toLowerCase()))
-                          .toList();
-                        }
-                    });
-                  },
-                            
-                            decoration: InputDecoration(
-                            contentPadding:
-                                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10),
-                            labelText: "Search by name",
-                            labelStyle: const TextStyle(fontWeight: FontWeight.bold,fontSize: 15),
-                            suffixIcon: const Icon(Icons.search,color: Color.fromARGB(255, 8, 4, 104),),
-                            // prefix: Icon(Icons.search),
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(width: 1, color: Colors.black),
-                              borderRadius: BorderRadius.circular(20),
-                              ),
-                             ),
-                           ),
-                         ),
-                         const SizedBox(width: 10),
-                          Expanded(
-                            flex: 2,
-                           child: Container(
-                           
-                              decoration: BoxDecoration(
-                                      border: Border.all(width:1 , color: Colors.black),
-                                      borderRadius: BorderRadius.circular(10.0),
-                                    ),
-                                child: GenderDropdown(
-                                genderOptions: ['All', 'Male', 'Female', 'Others'],
-                                onChanged: (selectedGender) {
-                                print('Selected Gender: $selectedGender');
-                                 },
-                              ),
-                           ),
-                         ),
-                       ],
-                     ),
-                     const SizedBox(height: 10),
-          //            if (filteredDoctorNames.isNotEmpty)
-          //            ListView.builder(
-          //   shrinkWrap: true,
-          //   itemCount: filteredDoctorNames.length,
-          //   itemBuilder: (context, index) {
-          //     return ListTile(
-          //       title: Text(filteredDoctorNames[index]),
-          //       onTap: () {
-          //         searchController.text = filteredDoctorNames[index];
-          //       },
-          //     );
-          //   },
-          // ),
-              //  ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: TextField(
+                    onChanged: (value) => _runFilter(value),
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 10),
+                      labelText: "Search by name",
+                      labelStyle: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 15),
+                      suffixIcon: const Icon(
+                        Icons.search,
+                        color: Color.fromARGB(255, 8, 4, 104),
+                      ),
+                      // prefix: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(width: 1, color: Colors.black),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.black),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: GenderDropdown(
+                      genderOptions: ['All', 'Male', 'Female', 'Others'],
+                      onChanged: (selectedGender) {
+                        print('Selected Gender: $selectedGender');
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            //            if (filteredDoctorNames.isNotEmpty)
+            //            ListView.builder(
+            //   shrinkWrap: true,
+            //   itemCount: filteredDoctorNames.length,
+            //   itemBuilder: (context, index) {
+            //     return ListTile(
+            //       title: Text(filteredDoctorNames[index]),
+            //       onTap: () {
+            //         searchController.text = filteredDoctorNames[index];
+            //       },
+            //     );
+            //   },
+            // ),
+            //  ),
             const SizedBox(height: 20),
             //   Stack(
             //     children: [ GridView.builder(
@@ -149,7 +155,7 @@ Widget build(BuildContext context) {
             //         },
             //       ),
             // ]),
-             Stack(
+            Stack(
               children: [
                 GridView.builder(
                   physics: const NeverScrollableScrollPhysics(),
@@ -165,27 +171,28 @@ Widget build(BuildContext context) {
                         doctors[index], index, index == selectedIndex);
                   },
                 ),
-                if (filteredDoctorNames.isNotEmpty)
+                if (_foundUsers.isNotEmpty)
                   Positioned(
                     top: 0,
                     left: 0,
                     right: 0,
                     child: Container(
-                      color: Color.fromARGB(255, 255, 255, 255).withOpacity(1.0),
+                      color:
+                          Color.fromARGB(255, 255, 255, 255).withOpacity(1.0),
                       padding: const EdgeInsets.all(8.0),
                       child: ListView.builder(
                         shrinkWrap: true,
-                        itemCount: filteredDoctorNames.length,
+                        itemCount: _foundUsers.length,
                         itemBuilder: (context, index) {
-                          final doctorName = filteredDoctorNames[index];
+                          final doctorName = _foundUsers[index]["name"]!;
                           final doctorBio = getDoctorBio(doctorName);
                           return ListTile(
                             leading: Icon(FontAwesomeIcons.userDoctor),
-                            title: Text(filteredDoctorNames[index]),
-                             subtitle: Text(doctorBio), 
+                            title: Text(_foundUsers[index]["name"]!),
+                            subtitle: Text(doctorBio),
                             onTap: () {
-                              searchController.text =
-                                  filteredDoctorNames[index];
+                              // searchController.text =
+                              //     _foundUsers[index]["name"]!;
                             },
                           );
                         },
@@ -197,65 +204,80 @@ Widget build(BuildContext context) {
           ],
         ),
       ),
-              
-            );
+    );
   }
-String getDoctorBio(String doctorName) {
-  
+
+  String getDoctorBio(String doctorName) {
     return "One-line bio of $doctorName";
   }
 
-
-List<Doctor> doctors = [
-  Doctor(name: "Dentist", icon: Icon(FontAwesomeIcons.tooth)),
-  Doctor(name: "Cardiologist", icon: Icon(FontAwesomeIcons.heartPulse , color: Color.fromARGB(255, 240, 26, 10),)),
-  Doctor(name: "Pulmonologist", icon: Icon(FontAwesomeIcons.lungs , color: Colors.pink)),
-  Doctor(name: "Oncologist", icon: Icon(FontAwesomeIcons.brain , color: Color.fromARGB(255, 240, 131, 167))),
-  Doctor(name: "General physician", icon: Icon(FontAwesomeIcons.stethoscope , color: Colors.black)),
-  Doctor(name: "Radiologist", icon: Icon(FontAwesomeIcons.xRay)),
-  Doctor(name: "Pediatrician", icon: Icon(FontAwesomeIcons.baby)),
-  Doctor(name: "Orthologist", icon: Icon(FontAwesomeIcons.bone , color: Color.fromARGB(255, 170, 169, 169))),
-  // Add more doctors here
-];
-Widget buildDoctorCard(Doctor doctor,index,bool selected) {
+  List<Doctor> doctors = [
+    Doctor(name: "Dentist", icon: Icon(FontAwesomeIcons.tooth)),
+    Doctor(
+        name: "Cardiologist",
+        icon: Icon(
+          FontAwesomeIcons.heartPulse,
+          color: Color.fromARGB(255, 240, 26, 10),
+        )),
+    Doctor(
+        name: "Pulmonologist",
+        icon: Icon(FontAwesomeIcons.lungs, color: Colors.pink)),
+    Doctor(
+        name: "Oncologist",
+        icon: Icon(FontAwesomeIcons.brain,
+            color: Color.fromARGB(255, 240, 131, 167))),
+    Doctor(
+        name: "General physician",
+        icon: Icon(FontAwesomeIcons.stethoscope, color: Colors.black)),
+    Doctor(name: "Radiologist", icon: Icon(FontAwesomeIcons.xRay)),
+    Doctor(name: "Pediatrician", icon: Icon(FontAwesomeIcons.baby)),
+    Doctor(
+        name: "Orthologist",
+        icon: Icon(FontAwesomeIcons.bone,
+            color: Color.fromARGB(255, 170, 169, 169))),
+    // Add more doctors here
+  ];
+  Widget buildDoctorCard(Doctor doctor, index, bool selected) {
     return GestureDetector(
-       onTap: () {
+      onTap: () {
+        setState(() {
+          selectedIndex = index;
+        });
 
-         setState(() {
-        selectedIndex = index;
-      });
-       
-      
         print('Hello ${index}');
       },
       child: Container(
-          margin: const EdgeInsets.all(0),
-          decoration: BoxDecoration(
-              border: Border.all(width: 1, color: Color.fromARGB(255, 4, 100, 178)), 
-              color: selected ? Colors.blue : Colors.white,
-              boxShadow: selected
-            ? [
-                BoxShadow(
-                  color: Color.fromARGB(255, 99, 179, 245).withOpacity(0.5), // Glow color
-                  spreadRadius: 5, // Spread radius for the glow effect
-                  blurRadius: 5, // Blur radius for the glow effect
-                ),
-              ]
-            : null,
-          ),
+        margin: const EdgeInsets.all(0),
+        decoration: BoxDecoration(
+          border: Border.all(width: 1, color: Color.fromARGB(255, 4, 100, 178)),
+          color: selected ? Colors.blue : Colors.white,
+          boxShadow: selected
+              ? [
+                  BoxShadow(
+                    color: Color.fromARGB(255, 99, 179, 245)
+                        .withOpacity(0.5), // Glow color
+                    spreadRadius: 5, // Spread radius for the glow effect
+                    blurRadius: 5, // Blur radius for the glow effect
+                  ),
+                ]
+              : null,
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             doctor.icon,
             SizedBox(height: 8),
-            Text(doctor.name , 
-             style: TextStyle(
-              color: selected ? Colors.white : Colors.black, // Change text color when selected
-            ),),
+            Text(
+              doctor.name,
+              style: TextStyle(
+                color: selected
+                    ? Colors.white
+                    : Colors.black, // Change text color when selected
+              ),
+            ),
           ],
         ),
       ),
     );
   }
-} 
-  
+}
